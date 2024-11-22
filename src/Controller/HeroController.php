34ad580b64e51c56d4d576240cac98f9d5596e2d
3thernity <3thernitydev@gmail.com>
2024-11-22 11:52:34 +0400

    add routes for list all heros

--- src/Controller/HeroController.php
+++ src/Controller/HeroController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\HeroRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HeroController extends AbstractController
 {
-    #[Route('/hero', name: 'app_hero')]
-    public function index(): Response
+    private HeroRepository $heroRepository;
+
+    public function __construct(HeroRepository $heroRepository)
     {
+        $this->heroRepository = $heroRepository;
+    }
+
+    // Routes pour voir toute les heros 
+    #[Route('/hero', name: 'app_hero')]
+    public function searchAll(): Response
+    {
+        $heros = $this->heroRepository->findAll();
+
         return $this->render('hero/index.html.twig', [
             'controller_name' => 'HeroController',
+            'heros' => $heros
+
         ]);
     }
 }
