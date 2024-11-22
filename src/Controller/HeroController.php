57769c5b35792ca70a97f75cd1dba57ee0bb8625
3thernity <3thernitydev@gmail.com>
2024-11-22 12:13:23 +0400

    change route

--- src/Controller/HeroController.php
+++ src/Controller/HeroController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/hero', name: 'hero_')]
 class HeroController extends AbstractController
 {
     private HeroRepository $heroRepository;
@@ -17,7 +18,7 @@ class HeroController extends AbstractController
     }
 
     // Routes pour voir toute les heros 
-    #[Route('/hero', name: 'app_hero')]
+    #[Route('/', name: 'hero_list')]
     public function searchAll(): Response
     {
         $heros = $this->heroRepository->findAll();
