de0607c6b5978ae497c72d96ea258441072166fb
3thernitydev <3thernitydev@gmail.com>
2024-11-25 00:46:41 +0400

    fix team entity

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TeamRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TeamRepository::class)]
@@ -16,8 +18,28 @@ class Team
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    /**
+     * @var Collection<int, Hero>
+     */
+    #[ORM\OneToMany(targetEntity: Hero::class, mappedBy: 'team')]
+    private Collection $hero;
+
+    #[ORM\Column]
+    private ?int $successRate = null;
+
+    #[ORM\Column]
+    private ?int $failRate = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $popularity = null;
+
     #[ORM\OneToOne(mappedBy: 'team', cascade: ['persist', 'remove'])]
     private ?Mission $mission = null;
+
+    public function __construct()
+    {
+        $this->hero = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -32,6 +54,72 @@ class Team
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Hero>
+     */
+    public function getHero(): Collection
+    {
+        return $this->hero;
+    }
+
+    public function addHero(Hero $hero): static
+    {
+        if (!$this->hero->contains($hero)) {
+            $this->hero->add($hero);
+            $hero->setTeam($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHero(Hero $hero): static
+    {
+        if ($this->hero->removeElement($hero)) {
+            // set the owning side to null (unless already changed)
+            if ($hero->getTeam() === $this) {
+                $hero->setTeam(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getSuccessRate(): ?int
+    {
+        return $this->successRate;
+    }
+
+    public function setSuccessRate(int $successRate): static
+    {
+        $this->successRate = $successRate;
+
+        return $this;
+    }
+
+    public function getFailRate(): ?int
+    {
+        return $this->failRate;
+    }
+
+    public function setFailRate(int $failRate): static
+    {
+        $this->failRate = $failRate;
+
+        return $this;
+    }
+
+    public function getPopularity(): ?string
+    {
+        return $this->popularity;
+    }
+
+    public function setPopularity(?string $popularity): static
+    {
+        $this->popularity = $popularity;
 
         return $this;
     }
