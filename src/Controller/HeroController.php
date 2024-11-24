80720d45e90ed872d1aaf4ea071c95f693e48b7f
3thernity <3thernitydev@gmail.com>
2024-11-24 14:57:07 +0400

    add power selector when creating a hero

--- src/Controller/HeroController.php
+++ src/Controller/HeroController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Hero;
 use App\Repository\HeroRepository;
+use App\Repository\PowerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,10 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HeroController extends AbstractController
 {
     private HeroRepository $heroRepository;
+    private PowerRepository $powerRepository;
     private EntityManagerInterface $em;
 
-    public function __construct(HeroRepository $heroRepository, EntityManagerInterface $em)
+    public function __construct(HeroRepository $heroRepository, EntityManagerInterface $em, PowerRepository $powerRepository)
     {
+        $this->powerRepository = $powerRepository;
         $this->heroRepository = $heroRepository;
         $this->em = $em;
     }
@@ -45,32 +48,47 @@ class HeroController extends AbstractController
 
 
     // Route pour créer un héros
+    // Route pour créer un héros
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function create(Request $request): Response
     {
         $hero = new Hero();
+        $powers = $this->powerRepository->findAll(); // Récupère tous les pouvoirs disponibles
 
         if ($request->isMethod('POST')) {
             $data = $request->request;
 
+            // Utilisation directe du PowerRepository
+            $power = $this->powerRepository->find($data->get('power')); // Récupère le pouvoir sélectionné
+
+            if (!$power) {
+                $this->addFlash('error', 'Pouvoir sélectionné invalide.');
+                return $this->redirectToRoute('hero_new');
+            }
+
             $hero->setName($data->get('name'))
-                ->setImage($data->get('image')) // Sous forme de lien temporairement || Rajout d'upload prévue 
+                ->setImage($data->get('image'))
                 ->setSecretIdendity($data->get('secretIdendity'))
                 ->setAge((int) $data->get('age'))
                 ->setNotableMission($data->get('notableMission'))
                 ->setSuccesRate((int) $data->get('succesRate'))
-                ->setFailRate((int) $data->get('failRate'));
+            ->setFailRate((int) $data->get('failRate'))
+                ->setPowerLink($power);
 
             $this->em->persist($hero);
             $this->em->flush();
 
+            $this->addFlash('success', 'Héros créé avec succès !');
             return $this->redirectToRoute('hero_list');
         }
 
         return $this->render('hero/new.html.twig', [
             'hero' => $hero,
+            'powers' => $powers, // Passe les pouvoirs au template
         ]);
     }
+
+
     // Route pour modifier un héro
     #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Hero $hero): Response
