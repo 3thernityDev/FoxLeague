f322e272bd6078af3264054118590996039c0631
3thernity <3thernitydev@gmail.com>
2024-11-27 15:07:58 +0400

    add five last mission to homePage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MissionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    public function index(MissionRepository $missionRepository): Response
     {
+        // Récupérer les 5 dernières missions (triées par date de création décroissante)
+        $missions = $missionRepository->findBy([], ['createDate' => 'DESC'], 5);
+
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'missions' => $missions,
         ]);
     }
 }
