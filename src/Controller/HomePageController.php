328da97525a0dcbbb4dd86380f25dc2bdffffbef
3thernity <3thernitydev@gmail.com>
2024-11-28 09:58:57 +0400

    add power listing

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MissionRepository;
+use App\Repository\PowerRepository; // Assurez-vous d'importer cette classe si elle est utilisée.
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,13 +11,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(MissionRepository $missionRepository): Response
+    public function index(MissionRepository $missionRepository, PowerRepository $powerRepository): Response
     {
         // Récupérer les 5 dernières missions (triées par date de création décroissante)
         $missions = $missionRepository->findBy([], ['createDate' => 'DESC'], 5);
 
+        // Récupérer tous les pouvoirs
+        $powers = $powerRepository->findAll();
+
+        // Organiser les pouvoirs par rareté
+        $groupedPowers = [];
+        foreach ($powers as $power) {
+            $rarity = $power->getRarity();
+            $groupedPowers[$rarity][] = $power;
+        }
+
         return $this->render('home_page/index.html.twig', [
             'missions' => $missions,
+            'groupedPowers' => $groupedPowers,
         ]);
     }
 }
