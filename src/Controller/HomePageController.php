aefc32fb527df63bb03801c0dcd254a63db50d40
3thernity <3thernitydev@gmail.com>
2024-11-29 09:12:00 +0400

    add top 3 of teams

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\TeamRepository;
 use App\Repository\MissionRepository;
 use App\Repository\PowerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(MissionRepository $missionRepository, PowerRepository $powerRepository): Response
+    public function index(MissionRepository $missionRepository, PowerRepository $powerRepository, TeamRepository $teamRepository): Response
     {
-        // Récupérer les 5 dernières missions (triées par date de création décroissante)
+        // Récupérer les 5 dernières missions
         $missions = $missionRepository->findBy([], ['createDate' => 'DESC'], 5);
 
         // Récupérer tous les pouvoirs
@@ -26,9 +27,16 @@ class HomePageController extends AbstractController
             $groupedPowers[$rarity][] = $power;
         }
 
+        // Récupérer toutes les équipes et trier par taux de succès (par exemple)
+        $teams = $teamRepository->findBy([], ['successRate' => 'DESC']);
+
+        // Sélectionner les 3 meilleures équipes
+        $topTeams = array_slice($teams, 0, 3);
+
         return $this->render('home_page/index.html.twig', [
             'missions' => $missions,
             'groupedPowers' => $groupedPowers,
+            'topTeams' => $topTeams,  // Passer les 3 meilleures équipes
         ]);
     }
 }
