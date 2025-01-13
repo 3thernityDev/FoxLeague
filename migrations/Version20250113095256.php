4d05f45cc0ce41a1bb39eea28554e32272b5a3d4
3thernitydev <3thernitydev@gmail.com>
2025-01-13 13:58:44 +0400

    add userEntity and registrationPage

--- migrations/Version20250113095256.php
+++ migrations/Version20250113095256.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241129042047 extends AbstractMigration
+final class Version20250113095256 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,14 @@ final class Version20241129042047 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE hero DROP notable_mission');
-        $this->addSql('ALTER TABLE mission DROP FOREIGN KEY FK_9067F23C296CD8AE');
-        $this->addSql('ALTER TABLE mission CHANGE team_id team_id INT NOT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE mission ADD CONSTRAINT FK_9067F23C296CD8AE FOREIGN KEY (team_id) REFERENCES team (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE hero ADD notable_mission VARCHAR(255) DEFAULT NULL');
+        $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE mission DROP FOREIGN KEY FK_9067F23C296CD8AE');
-        $this->addSql('ALTER TABLE mission CHANGE team_id team_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE mission ADD CONSTRAINT FK_9067F23C296CD8AE FOREIGN KEY (team_id) REFERENCES team (id)');
     }
 }
